6e2d4a5871df6703fd363cf2da80e7cce9db2b9e
Ubuntu <ubuntu@ip-172-31-47-137.ec2.internal>
2019-11-06 14:15:44 +0000

    Updated app.py to test flash message appearance on heroku.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, get_flashed_messages, jsonify 
+from flask import Flask, render_template, request, redirect, url_for, flash,  jsonify #get_flashed_messages,
 import smtplib
 import os
 import pymongo
